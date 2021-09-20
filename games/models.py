03a35a0bdc68d492f9bc9df7b347a0b34a4ab6c3
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-09-20 16:04:04 +0000

    [ADD] JSON data files added

--- games/models.py
+++ games/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    # Fields that correspond to "categories.json" file
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    # Takes in the category model itself
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Game(models.Model):
+    # Foreign key to the category model
+    # We'll allow this to be null in the database and blank in forms
+    # and if a category is deleted we'll set any products that use it to have null for this field
+    # rather than deleting the product.
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+
+    
