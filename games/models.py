3ffb61d56167363e8ef26e42bfbd5c08225739cc
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-09-20 16:32:33 +0000

    [ADD] Added game models and images

--- games/models.py
+++ games/models.py
@@ -19,5 +19,14 @@ class Game(models.Model):
     # and if a category is deleted we'll set any products that use it to have null for this field
     # rather than deleting the product.
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    genre = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
     
