703b92bd94ae4ec4459b3200d1ec0b5dbd2725d7
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-09-20 17:22:55 +0000

    [ADD] Game details page added

--- games/views.py
+++ games/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Game
 
 # Create your views here.
@@ -12,3 +12,15 @@ def all_games(request):
     }
 
     return render(request, "games/games.html", context)
+
+
+def game_detail(request, game_id):
+    """ A view to show a game's details """
+
+    game = get_object_or_404(Game, pk=game_id)
+
+    context = {
+        'game': game,
+    }
+
+    return render(request, 'games/game-detail.html', context)
