6724f9004e19e2dc0c4f0bbb19da4a24b43f27db
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-09-20 16:43:03 +0000

    [REFACTOR] Changes to admin

--- games/admin.py
+++ games/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Game, Category
 
 # Register your models here.
-admin.site.register(Game)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'genre',
+        'description',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+admin.site.register(Game, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
