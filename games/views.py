6ea7c5a50fb7a338969327b4dcdac3671b95bcdf
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-09-22 12:21:41 +0000

    [ADD] Sort functionality

--- games/views.py
+++ games/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Game, Category
 
 # Create your views here.
@@ -10,8 +12,24 @@ def all_games(request):
     games = Game.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                games = games.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            games = games.order_by(sortkey)
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             games = games.filter(category__name__in=categories)
@@ -26,10 +44,13 @@ def all_games(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             games = games.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'games': games,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'games/games.html', context)
