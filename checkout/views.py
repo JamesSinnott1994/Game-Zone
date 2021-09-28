8e77a23a6333cf15e76f61ae17fe06add23b53c7
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-09-28 17:32:09 +0000

    [ADD] Stripe elements added

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JMuYtKGLMUHtvCLMfvpbr9GiPoX3xNDYEbWJrJXY3rAlxyJKJQDMBMjX5RJ5ecw9iWrUlBOcrxZS1JOQayECbfP00edz2rnj0',
+        'client_secret': 'test',
     }
 
     return render(request, template, context)
