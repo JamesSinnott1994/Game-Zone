31bd5481d1c82e9bc2aa3aec077adace881c04c8
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-10-06 10:37:52 +0000

    [ADD] Wishlist items displayed for each user

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_list_or_404, get_object_or_404
 from .models import Game, UserProfile, Wishlist, WishlistItem
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def wishlist(request):
-    return render(request, 'wishlist/wishlist.html')
+    user = get_object_or_404(UserProfile, user=request.user)
+    print(user)
+
+    wishlist = Wishlist.objects.get_or_create(user=user)
+    wishlist_user = wishlist[0]
+
+    wishlist_exists = WishlistItem.objects.filter(wishlist=wishlist_user).exists()
+
+    print(wishlist_user)
+
+    games = []
+    if wishlist_exists:
+        user_wishlist = get_list_or_404(WishlistItem, wishlist=wishlist_user)
+        for obj in user_wishlist:
+            game = get_object_or_404(Game, name=obj)
+            games.append(game)
+        context = {
+            'wishlist': True,
+            'games': games
+        }
+        print("GAME IN WISHLIST")
+        return render(request, 'wishlist/wishlist.html', context)
+
+    else:
+        context = {
+            'wishlist': False,
+        }
+        print("GAME NOT IN WISHLIST")
+        return render(request, 'wishlist/wishlist.html', context)
 
 
+@login_required
 def add_to_wishlist(request, game_id):
     redirect_url = request.POST.get('redirect_url')
 
@@ -19,9 +50,9 @@ def add_to_wishlist(request, game_id):
     game = Game.objects.get(pk=game_id)
 
     if request.POST:
-        test = WishlistItem.objects.filter(wishlist=wishlist_user, game=game).exists()
-        if test:
-            messages.error(request, "Product already in your wishlist")
+        game_in_wishlist = WishlistItem.objects.filter(wishlist=wishlist_user, game=game).exists()
+        if game_in_wishlist:
+            messages.error(request, "Game already in your wishlist")
             return redirect(redirect_url)
 
         else:
