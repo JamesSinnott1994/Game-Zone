a9dc86d2565c3c36ffb43343d04fae41adbc2207
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-10-13 14:07:21 +0000

    [ADD] Finishing Heroku deployment

--- game_zone/settings.py
+++ game_zone/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^cvpv(11b=0-kvb3%)*kwn^*1pph-z-tnxj=)c+r5-m2oolgcm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['js-game-zone.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -100,12 +100,17 @@ WSGI_APPLICATION = 'game_zone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
